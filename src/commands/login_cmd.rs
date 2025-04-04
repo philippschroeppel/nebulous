4cf118b63d03ce3578e1ef196ae7dca378bacfdb
Kilian Kluge <dev@kluge.ai>
2025-04-04 21:38:59 +0200

    fix: add api_key for nebu-only config

--- src/commands/login_cmd.rs
+++ src/commands/login_cmd.rs
@@ -5,23 +5,23 @@ use nebulous::config::{GlobalConfig, ServerConfig};
 use open;
 use rpassword;
 
-pub async fn execute(url: String, auth: Option<String>, hub: Option<String>) -> Result<(), Box<dyn Error>> {
+pub async fn execute(nebu_url: String, auth: Option<String>, hub: Option<String>) -> Result<(), Box<dyn Error>> {
     if auth.is_none() ^ hub.is_none() {
-        eprintln!("Either auth or hub URL provided. Please provide both.");
+        eprintln!("Either auth or hub URL provided. Please provide both or neither.");
         return Ok(());
     }
 
     let mut config = GlobalConfig::read()?;
 
     if auth.is_some() && hub.is_some() {
-        let auth_address = auth.unwrap();
-        let hub_address = hub.unwrap();
+        let auth_url = auth.unwrap();
+        let hub_url = hub.unwrap();
 
-        let url = format!("{}/settings/api", hub_address);
-        println!("\nVisit {} to get an API key\n", url);
+        let hub_api_url = format!("{}/settings/api", hub_url);
+        println!("\nVisit {} to get an API key\n", hub_api_url);
 
         // Attempt to open the URL in the default browser
-        if let Err(e) = open::that(&url) {
+        if let Err(e) = open::that(&hub_api_url) {
             eprintln!("Failed to open browser: {}", e);
         }
 
@@ -31,17 +31,22 @@ pub async fn execute(url: String, auth: Option<String>, hub: Option<String>) -> 
 
         config.servers.push(ServerConfig {
             name: Some("cloud".to_string()),
-            server: Some(url),
+            server: Some(nebu_url),
             api_key: Some(api_key),
-            auth_server: Some(auth_address),
+            auth_server: Some(auth_url),
         });
         config.current_server = Some("cloud".to_string());
     } else {
+
+        let auth_url = format!("{}/auth", nebu_url);
+
+        // TODO: Get an API key
+
         config.servers.push(ServerConfig {
             name: Some("nebu".to_string()),
-            server: Some(url),
+            server: Some(nebu_url),
             api_key: None,
-            auth_server: None,
+            auth_server: Some(auth_url),
         });
         config.current_server = Some("nebu".to_string());
     }
